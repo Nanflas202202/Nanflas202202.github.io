79d98fe517df753cda544815503cefa8fed2537f
Nanflas202202 <121420255+Nanflas202202@users.noreply.github.com>
2022-12-26 16:17:10 +0800

    Add files via upload

---
+++ NanflasCY2BStudio/Download/VirusTrainer-alpha31-Rebuild-Final-Edition-没bug打死绝对不改了版.cpp
@@ -0,0 +1,106 @@
+#include<windows.h>
+#include<iostream>
+//为了省事，using namespace std就不写了
+//王致远，你是强迫症的话就自己加上吧
+using namespace std;
+int main(){
+    //这个程序是高仿老版的，但将bat->exe改成了用std::system一行行执行，以免杀软误报
+    //我也不知道为什么用Quick Batch Compiler编译出来的软件那么多杀软会报毒
+    //3
+    //2
+    //1,
+    //Go!                                                         
+    std::cout<<"                                ..,   hp@DESKTOP-K5KAMNT"<<std::endl;
+    std::cout<<"                    ....,,:;+ccllll   ------------------"<<std::endl;
+    std::cout<<"      ...,,+:;  cllllllllllllllllll   OS: Windows 10 רҵ▒▒ x86_64"<<std::endl;
+    std::cout<<",cclllllllllll  lllllllllllllllllll   Host: HP HP 348 G7"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   Kernel: 10.0.19044"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   Uptime: 1 day, 3 hours, 25 mins"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   Packages: 1546 (cygcheck)"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   Shell: bash 4.4.12"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   Resolution: 1920x1080"<<std::endl;
+    std::cout<<"                                      DE: Aero"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   WM: Explorer"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   WM Theme: Custom"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   Terminal: mintty"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   CPU: Intel i5-10210U (8) @ 2.120GHz"<<std::endl;
+    std::cout<<"llllllllllllll  lllllllllllllllllll   GPU: Caption"<<std::endl;
+    std::cout<<"`'ccllllllllll  lllllllllllllllllll   GPU: Radeon 530 Series"<<std::endl;
+    std::cout<<"       `' \*::  :ccllllllllllllllll   GPU: Intel(R) UHD Graphics"<<std::endl;
+    std::cout<<"                       ````''*::cll   GPU"<<std::endl;
+    std::cout<<"                                 ``   Memory: 6672MiB / 8014MiB"<<std::endl;
+    /*std::cout<<"hp@DESKTOP-K5KAMNT ~/Neofetch
+    std::cout<<"                                                        Class2@NFLS Grade 7"<<std::endl;
+    std::cout<<"                       (#####                            ------------------"<<std::endl;
+    std::cout<<"                     (((########  #####                  OS: Windows 10 x86_64"<<std::endl;
+    std::cout<<"                    (((        ##########    __...__     Host: HP HP 348 G7"<<std::endl;
+    std::cout<<"             ((((((((           #######    /((((((###\   Kernel: 10.0.19044"<<std::endl;
+    std::cout<<"           (((((((((((   .......           \(((((####/   Uptime: 5 days, 20 hours, 1"<<std::endl;
+    std::cout<<"          ((((((    ((((#########            *******     Packages: 1546 (cygcheck)"<<std::endl;
+    std::cout<<"    %((((((#          ((########                         Shell: bash 4.4.12"<<std::endl;
+    std::cout<<" /////(((((              ###                             Resolution: 1920x1080"<<std::endl;
+    std::cout<<"/////(((((((#   (((&                                     DE: Aero"<<std::endl;
+    std::cout<<"         (((((((((((((                                   WM: Explorer"<<std::endl;
+    std::cout<<"          ((((((((((((                                   WM Theme: Pride 201"<<std::endl;
+    std::cout<<"           (((((((((     ((((((###                       Terminal: mintty"<<std::endl;
+    std::cout<<"                       /((((((######                     CPU: Intel i5-10210U (8) @"<<std::endl;
+    std::cout<<"                      //((((((######                     GPU: Caption"<<std::endl;
+    std::cout<<"                       /((((((#####                      GPU: Radeon 530 Series"<<std::endl;
+    std::cout<<"                        *********//*                       GPU: Intel(R) UHD Graphics"<<std::endl;
+    std::cout<<"                                                         GPU"<<std::endl;
+    std::cout<<"                                                         Memory: 6540MiB / 8014MiB"<<std::endl;*/
+    std::system("COLOR 02");
+    system("title New NFLS Virus Trainer Alpha 3.1");
+    std::cout<<"          Welcome to NFLS Virus Trainer Alpha 3.1 SP1 Rebuild!"<<std::endl;
+    std::cout<<"----------------Killing Viruses----------------"<<std::endl;
+	std::cout<<"---------------Killing Processes---------------"<<std::endl;
+	std::system("TASKKILL -F -IM Rundll32.exe -T");
+			    std::system("TASKKILL -F -IM AvastSvc.exe -T");
+				std::system("TASKKILL -F -IM wscript.exe -T");
+				std::system("TASKKILL -F -IM Autolt3.exe -T");
+				std::system("TASKKILL -F -IM cmd.exe -T");
+				std::system("TASKKILL -F -IM rundll32.exe -T");
+				cout<<"----------------------FINISH-------------------"<<endl;
+				cout<<"-------------Showing Hidden Files--------------"<<endl;
+		    	std::system("ATTRIB -S -H E:\* /d /l");
+				std::system("ATTRIB -S -H F:\* /d /l");
+				std::system("ATTRIB -S -H G:\* /d /l");	
+				std::system("ATTRIB -S -H I:\* /d /l");
+				std::system("ATTRIB -S -H J:\* /d /l");
+				std::system("ATTRIB -S -H K:\* /d /l");
+				cout<<"----------------------FINSIH-------------------"<<endl;
+				std::system("ren E:\*.lnk *.vir");
+				std::system("ren F:\*.lnk *.vir");
+				std::system("ren G:\*.lnk *.vir");
+				std::system("ren H:\*.lnk *.vir");
+				std::system("ren I:\*.lnk *.vir");
+				std::system("ren J:\*.lnk *.vir");
+				std::system("ren K:\*.lnk *.vir");
+				std::system("ren E:\RECYCLER.BIN VirusMainFiles");
+				std::system("ren F:\RECYCLER.BIN VirusMainFiles");
+				std::system("ren G:\RECYCLER.BIN VirusMainFiles");
+				std::system("ren H:\RECYCLER.BIN VirusMainFiles");
+				std::system("ren I:\RECYCLER.BIN VirusMainFiles");
+				std::system("ren J:\RECYCLER.BIN VirusMainFiles");
+				std::system("ren K:\RECYCLER.BIN VirusMainFiles");
+				std::system("echo Disabling Autorun");
+				std::system("cd C:\ProgramData\AvastSvcpCP");
+				std::system("ren C:\ProgramData\AvastSvcpCP\Avastsvc.exe Avastsvc.exe.vf");
+				std::system("ren C:\ProgramData\AvastSvcpCP\Avastsvc.dat Avastsvc.dat.vf");
+				std::system("ren C:\ProgramData\AvastSvcpCP\wsc.dll wsc.dll.vf");
+				std::system("ren C:\Google\Autolt3.exe AL3ScriptRunner.exe");
+				std::cout<<"----------------------FINSIH-------------------"<<std::endl;
+			cout<<"-------------Showing Hidden Files--------------"<<endl;
+			system("ATTRIB -S -H E:\ /d /l");
+			system("ATTRIB -S -H F:\ /d /l");
+			system("ATTRIB -S -H G:\ /d /l");	
+			system("ATTRIB -S -H I:\ /d /l");
+			system("ATTRIB -S -H J:\ /d /l");
+			system("ATTRIB -S -H K:\ /d /l");
+			std::cout<<"----------------------FINSIH-------------------"<<endl;
+			std::cout<<"----------------------FINSIH-------------------"<<endl;
+			std::cout<<"Press <Enter> to continue..."<<endl;
+			getchar();
+			system("TASKKILL -F -IM conhost.exe -T");
+			return 0;
+}
